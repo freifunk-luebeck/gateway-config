4f5d591187cd9c74ffc29a61214442e6f39ff519
Philipp Rothmann <philipprothmann@posteo.de>
2021-11-09 22:18:57 +0100

    fix dns-update service

---
+++ roles/base/files/scripts/update-dns.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+DIR=$(mktemp -d)
+DEST=/var/lib/powerdns/zones
+REPO=https://git.luebeck.freifunk.net/FreifunkLuebeck/ffhl-dns.git
+
+mkdir -p "$DEST"
+git clone "$REPO" "$DIR"
+git --git-dir="$DIR/.git" --work-tree="$DEST" reset --hard
+
+rm -rf $DIR
+
+pdns_control reload
