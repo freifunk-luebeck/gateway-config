6db7d81a9b418a651db23814ae1f76b11b73fbee
Paul Maruhn <paulmaruhn@posteo.de>
2022-01-04 16:43:14 +0100

    wip: add auth_ns role

---
+++ roles/ffhl_ns_auth/templates/update-dns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+DIR=$(mktemp -d)
+DEST="/var/local/ffhl-dns"
+REPO="{{ dns_repo_url }}"
+
+mkdir -p "$DEST"
+git clone "$REPO" "$DIR"
+git --git-dir="$DIR/.git" --work-tree="$DEST" reset --hard
+
+rm -rf "$DIR"
+
+chown -R bind:bind "$DEST"
+
+# if this script is started by ansible, named was restarted just before this service
+# we need to give named some time to startup
+sleep 5
+
+systemctl reload named.service
